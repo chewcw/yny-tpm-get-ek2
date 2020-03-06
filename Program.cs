fbd03daee5ddddf175022bdc73fa94562d5dc600
chewcw <chwai87@gmail.com>
2020-03-06 17:03:19 +0800

    add CLI arguments

--- Program.cs
+++ Program.cs
@@ -3,30 +3,56 @@ using Microsoft.Azure.Devices.Provisioning.Client.Transport;
 using Microsoft.Azure.Devices.Provisioning.Security;
 using Microsoft.Azure.Devices.Shared;
 using System;
+using McMaster.Extensions.CommandLineUtils;
 
 namespace Microsoft.Azure.Devices.Provisioning.Client.Samples
 {
-    public static class Program
+    [Subcommand(typeof(GetTPM))]
+    [HelpOption]
+    class Program
     {
-        private const string RegistrationId = "";
-        public static int Main(string[] args)
+        public static void Main(string[] args)
+        {
+            CommandLineApplication.Execute<Program>(args);
+        }
+
+        private int OnExecute()
+        {
+            Console.Error.WriteLine("No command passed to the command line. Use --help option to check what arguments available.");
+            return -1;
+        }
+    }
+
+    [HelpOption]
+    class GetTPM
+    {
+        [Option("-r", LongName = "Registration", Description = "Regisration ID")]
+        private string RegistrationId { get; set; }
+
+        public int OnExecute()
         {
             try
             {
+                if (RegistrationId == "" || RegistrationId == null)
+                {
+                    RegistrationId = Prompt.GetString("Please type the RegistrationID: ", null, ConsoleColor.Red, ConsoleColor.Black);
+                }
+
+
                 using (var security = new SecurityProviderTpmHsm(RegistrationId))
-                using (var transport = new ProvisioningTransportHandlerHttp())
                 {
                     string base64EK = Convert.ToBase64String(security.GetEndorsementKey());
                     Console.WriteLine(base64EK);
                 }
 
+                return 0;
+
             }
             catch (Exception err)
             {
                 Console.WriteLine($@"Error: {err}");
+                return -1;
             }
-
-            return 0;
         }
     }
 }
